f6645dedbd9fc89c3d7dafb1821089fd26f51a7a
dandankarai <cunhacb22@gmail.com>
2021-01-04 11:07:49 -0300

    usando uma maneira mais simples de gerar os números

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -18,14 +18,26 @@ export default class Mega extends Component {
     return nums.includes(novo) ? this.gerarNumeroNaoContido(nums) : novo
   }
 
+  // gerarNumeros = () =>{
+  //   const numeros = Array(this.state.qtdeNumeros)
+  //     .fill()
+  //     .reduce(n => [...n, this.gerarNumeroNaoContido(n)], [])
+  //     .sort((a,b) => a - b) //ordem crescente
+  //   this.setState({numeros})
+  // }
+
   gerarNumeros = () =>{
-    const numeros = Array(this.state.qtdeNumeros)
-      .fill()
-      .reduce(n => [...n, this.gerarNumeroNaoContido(n)], [])
-      .sort((a,b) => a - b) //ordem crescente
+    const {qtdeNumeros} = this.state
+    const numeros = []
+
+    for (let i = 0; i < qtdeNumeros; i++){
+      const n = this.gerarNumeroNaoContido(numeros)
+      numeros.push(n)
+    }
+    numeros.sort((a,b) => a - b)
     this.setState({numeros})
   }
-
+  
   render() {
     return (
       <>
